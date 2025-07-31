20e13952b495632114608ea4049e0aecaedebd6e
vlcodge <beegooglsteam@gmail.com>
2025-07-31 20:03:43 +0300

    DbContextNew

--- Infrastructure/EntityFramework/DbContext.cs
+++ Infrastructure/EntityFramework/DbContext.cs
@@ -4,12 +4,12 @@ namespace SoftBank.Infrastructure.EntityFramework;
 
 public class SoftBankDbContext : DbContext
 {
-    public SoftBankDbContext()
+    public SoftBankDbContext(DbContextOptions<SoftBankDbContext> options)
+        : base(options)
     {
-
     }
+
     public DbSet<UserEntity> Users { get; set; }
     public DbSet<CardEntity> Cards { get; set; }
     public DbSet<TransactionEntity> Transactions { get; set; }
-
 }
