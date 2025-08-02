63aa4869fe68cd8c88d6e5947a1a7000777cded4
KilluaZoludyck <144722702+KilluaZoludyck@users.noreply.github.com>
2025-08-02 17:28:23 +0300

    CreatingUserRep

--- Core/Repositories/IUserRepository.cs
+++ Core/Repositories/IUserRepository.cs
@@ -1,7 +1,8 @@
+using SoftBank.Core.Repositories;
+using SoftBank.Shared.Dto;
 namespace SoftBank.Core.Repositories;
 
-public interface IUserRepository : IBaseRepository
+public interface IUserRepository : IRepository<UserDto, Guid>
 {
     void UpdateUserBalance(Guid userId, decimal balance);
 }
-
