7fda9070197d9d8eb7e05e143d12f47be74c2290
vlvsct <beegooglsteam@gmail.com>
2025-08-19 14:16:24 +0000

    ClientService -> ClientBFFService

---
+++ Core/Services/Bff/ClientBFFService.cs
@@ -0,0 +1,10 @@
+using SoftBank.Shared.Dto;
+using SoftBank.Shared.Model;
+using SoftBank.Core.Services.Interfaces;
+namespace SoftBank.Core.Services.BFF;
+
+public class ClientBFFService : IClientBFFService
+{
+    
+
+}
