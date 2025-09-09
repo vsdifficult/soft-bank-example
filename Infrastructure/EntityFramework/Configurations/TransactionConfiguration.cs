bc5322c2684df5b8193bdd7c10a7e9db59e1a160
vsdifficult <linchenko017@gmail.com>
2025-09-09 20:13:20 +0300

    Update

--- Infrastructure/EntityFramework/Configurations/TransactionConfiguration.cs
+++ Infrastructure/EntityFramework/Configurations/TransactionConfiguration.cs
@@ -9,21 +9,7 @@ namespace SofBank.Infrastructure.EntityFramework.Repositories
         public void Configure(EntityTypeBuilder<TransactionEntity> builder)
         {
             builder.HasKey(t => t.Id);
-
-
-            builder.HasOne(t => t.Sender)
-                .WithMany(u => u.SentTransactions) 
-                .HasForeignKey(t => t.SenderId);
-
-
-            builder.HasOne(t => t.Recipient)
-                .WithMany(u => u.ReceivedTransactions)
-                .HasForeignKey(t => t.RecipientId);
-
-
-            builder.HasOne(t => t.Card)
-                .WithMany(c => c.Transaction)
-                .HasForeignKey(t => t.FromCardId); 
+            
         }
     }
 }
